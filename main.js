a0580136abeef42d0bc3e538ac96df58744e967b
soposilverhawk <Darkness24lol@gmail.com>
2022-07-29 12:59:02 +0400

    finished

---
+++ main.js
@@ -0,0 +1,5 @@
+let button = document.querySelector("button")
+let container = document.querySelector(".container")
+button.addEventListener("click", function(){
+    container.classList.toggle("color-change")
+})
